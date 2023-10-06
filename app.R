521af518d6547ae913756b1b5d3765aa7ccdd259
Juan Correa <juancorrea@iMac-de-Juan.local>
2023-10-06 04:38:47 -0500

    add reactive button

--- app.R
+++ app.R
@@ -7,8 +7,8 @@
 #    http://shiny.rstudio.com/
 #
 
-# don't worry about it! It have 1USD limit.
-Sys.setenv(OPENAI_API_KEY = "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")
+# don't worry about it! It has a 1USD limit.
+Sys.setenv(OPENAI_API_KEY = "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")
 
 library(chatgpt)
 library(shiny)
@@ -29,18 +29,19 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            numericInput(inputId = "IMC", label = "ingresa tu indice de masa corporal", value = 5, min = 5, max = 200),
-            numericInput(inputId = "Glucose", label = "ingresa tu ultima toma de glucosa", value = 5, min = 1, max = 2),
-            numericInput(inputId = "AvBloodPressure", label = "ingresa tu ultima toma de persión sanguinea ", value = 5, min = 1, max = 2),
-            numericInput(inputId = "SpO2", label = "ingresa tu ultima toma de oximetria", value = 5, min = 1, max = 2),
-            numericInput(inputId = "HeartRate", label = "ingresa tu ultima toma de frecuencia cardiaca", value = 5, min = 1, max = 2),
+            numericInput(inputId = "IMC", label = "ingresa tu indice de masa corporal", value = 30, min = 5, max = 200),
+            numericInput(inputId = "Glucose", label = "ingresa tu ultima toma de glucosa", value = 100, min = 1, max = 2),
+            numericInput(inputId = "AvBloodPressure", label = "ingresa tu ultima toma de persión sanguinea ", value = 100, min = 1, max = 2),
+            numericInput(inputId = "SpO2", label = "ingresa tu ultima toma de oximetria", value = 100, min = 1, max = 2),
+            numericInput(inputId = "HeartRate", label = "ingresa tu ultima toma de frecuencia cardiaca", value = 100, min = 1, max = 2),
+            actionButton("do", "Verificar!")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
           h1("Predicción paciente vulnerable a diabetes."),
           shinycssloaders::withSpinner(
-            textOutput("predict")
+             textOutput("predict")
           )
         )
     )
@@ -49,11 +50,13 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
+    gpt <- eventReactive(input$do, {
+      prompt <- "Actúa como doctor. Genera algunas sugerencias para evitar la diabetes. La sugerencia debe ser corta y centrarse en el objetivo principal del tratamiento: reducir el riesgo a diabetes. Se lo estás diciendo a un paciente."
+      ask_chatgpt(prompt)
+    })
+
     output$predict <- renderText({
-      
-      prompt <- "Actúa como doctor. Genera algunas sugerencias para evitar la diabetes. La sugerencia debe ser corta y centrarse en el objetivo principal del tratamiento: reducir el riesgo a diabetes. Se lo estás diciendo a un paciente." 
-      suggestion <- ask_chatgpt(prompt)
-      
+
       cluster <- predict(model, newdata = data.frame(
          IMC = input$IMC,
          Glucose = input$Glucose,
@@ -62,14 +65,14 @@ server <- function(input, output) {
          HeartRate = input$SpO2
        ), type = "class"
       )
-      
+
       ifelse(
         cluster == 1,
-        paste("Paciente con vulnerabilidad a diabetes. ", suggestion), 
-        paste("Paciente dentro de los parametros normales.", suggestion)
+        paste("Paciente con vulnerabilidad a diabetes. ", gpt()),
+        paste("Paciente dentro de los parametros normales.", gpt())
       )
     })
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
